185e972e5a79f69f3ba44af08b5d1ce5e022b751
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2024-03-07 15:03:35 -0500

    hw-7 v1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import ContactForm from './ContactForm/';
+import ContactsList from './ContactsList';
+import s from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.app}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+
+      <ContactsList />
+      
     </div>
   );
 };
+
+/*
+[
+      { id: 'id-1', name: 'David Beckham', number: '111-111-1111' },
+      { id: 'id-2', name: 'Luis Suárez', number: '222-222-2222' },
+      { id: 'id-3', name: 'Mohamed Salah', number: '333-333-3333' },
+      { id: 'id-4', name: 'Virgil van Dijk', number: '444-444-4444' },
+      { id: 'id-5', name: 'Jurgen Klopp', number: '555-555-5555' },
+      { id: 'id-6', name: 'Jamie Carragher', number: '666-666-2323' },
+      { id: 'id-7', name: 'James Milner', number: '777-777-7777' },
+      { id: 'id-8', name: 'Steven Gerrard', number: '888-888-8888' },
+      { id: 'id-9', name: 'Frank Lampard', number: '999-999-9999' },
+      { id: 'id-10', name: 'Xabi Alonso', number: '111-999-9999' },
+    ]
+*/
---
+++ src/redux/contactsOperation.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = 'https://65e9dd7cc9bf92ae3d3a7a0f.mockapi.io';
+
+export const getContacts = createAsyncThunk('/contacts/fetchAll', 
+async (_, thunkAPI) => {
+    try {
+        const {data} = await axios.get('/contacts/contacts');
+       
+        return data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+}
+)
+
+export const addContact = createAsyncThunk('/contacts/addContact', 
+async (contact, thunkAPI) => {
+    try {
+        const {data} = await axios.post('/contacts/contacts', {
+            name: contact.name,
+            phone: contact.number
+        })
+        return data
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+}
+)
+
+export const deleteContact = createAsyncThunk('/contacts/deleteContact', 
+async (id, thunkAPI) => {
+    try {
+         await axios.delete(`/contacts/contacts/${id}`)
+         
+        return id
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+}
+)
